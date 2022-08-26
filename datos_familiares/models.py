701071c347b6876ec6efec292bb608dff49acffa
troy1950 <cristianivan21@hotmail.com>
2022-08-26 22:18:26 +0200

    Primera version proyecto Entregable

---
+++ datos_familiares/models.py
@@ -0,0 +1,16 @@
+from pyexpat import model
+from django.db import models
+
+# Create your models here.
+
+class Datos(models.Model):
+    nombre = models.CharField(max_length=128)
+    fecha_nacimiento = models.DateField()
+    peso_kg = models.IntegerField()
+    
+
+
+class Interesante(models.Model):
+    profesion = models.CharField(max_length=128)
+    hobbys = models.CharField(max_length=128)
+    proyecto_familia = model.foreignKey('Datos', on_delete=models.CASCADE)
