9ed94ac80b6eae75428e814c1d6e12f5d49dcfa9
troy1950 <cristianivan21@hotmail.com>
2022-08-26 23:03:53 +0200

    nuevos

--- datos_familiares/models.py
+++ datos_familiares/models.py
@@ -1,4 +1,4 @@
-from pyexpat import model
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
@@ -7,10 +7,15 @@ class Datos(models.Model):
     nombre = models.CharField(max_length=128)
     fecha_nacimiento = models.DateField()
     peso_kg = models.IntegerField()
+
+    pass
     
 
 
 class Interesante(models.Model):
     profesion = models.CharField(max_length=128)
     hobbys = models.CharField(max_length=128)
-    proyecto_familia = model.foreignKey('Datos', on_delete=models.CASCADE)
+    proyecto_familia = models.ForeignKey('Datos', on_delete=models.CASCADE)
+
+
+    pass
